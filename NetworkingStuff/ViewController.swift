354304b0ce59a468623a19e5c3dd4f0f60ee66eb
Skullbonk <reagan.fairbanks@gmail.com>
2018-12-12 13:53:22 -0700

    Initial commit

---
+++ NetworkingStuff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkingStuff
+//
+//  Created by Fairbanks, Reagan on 12/12/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
